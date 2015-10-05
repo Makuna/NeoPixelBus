f37b617d47b8d44f7a16a9e2df02b37df6c7be6b
Matthew <matthew.ldurr@gmail.com>
2015-10-05 10:54:52 -0400

    fix indentation and braces

--- examples/NeoPixelTest/NeoPixelTest.ino
+++ examples/NeoPixelTest/NeoPixelTest.ino
@@ -26,55 +26,53 @@ HslColor hslBlue( blue );
 HslColor hslWhite( white );
 HslColor hslBlack( black );
     
-void setup()
-{
-    // this resets all the neopixels to an off state
-    strip.Begin();
-    strip.Show();
+void setup(){
+  // this resets all the neopixels to an off state
+  strip.Begin();
+  strip.Show();
 }
 
 
-void loop()
-{
-    delay(1000);
+void loop(){
+  delay(1000);
 
-    // set the colors, 
-    // if they don't match in order, you may need to use NEO_GRB flag
-    strip.SetPixelColor(0, red);
-    strip.SetPixelColor(1, green);
-    strip.SetPixelColor(2, blue);
-    strip.SetPixelColor(3, white);
-    strip.Show();
+  // set the colors, 
+  // if they don't match in order, you may need to use NEO_GRB flag
+  strip.SetPixelColor(0, red);
+  strip.SetPixelColor(1, green);
+  strip.SetPixelColor(2, blue);
+  strip.SetPixelColor(3, white);
+  strip.Show();
 
-    
-    delay(3000);
+  
+  delay(3000);
 
-    // turn off the pixels
-    strip.SetPixelColor(0, black);
-    strip.SetPixelColor(1, black);
-    strip.SetPixelColor(2, black);
-    strip.SetPixelColor(3, black);
-    strip.Show();
-    
-        delay(1000);
+  // turn off the pixels
+  strip.SetPixelColor(0, black);
+  strip.SetPixelColor(1, black);
+  strip.SetPixelColor(2, black);
+  strip.SetPixelColor(3, black);
+  strip.Show();
+  
+      delay(1000);
 
-    // set the colors, 
-    // if they don't match in order, you may need to use NEO_GRB flag
-    strip.SetPixelColor(0, hslRed);
-    strip.SetPixelColor(1, hslGreen);
-    strip.SetPixelColor(2, hslBlue);
-    strip.SetPixelColor(3, hslWhite);
-    strip.Show();
+  // set the colors, 
+  // if they don't match in order, you may need to use NEO_GRB flag
+  strip.SetPixelColor(0, hslRed);
+  strip.SetPixelColor(1, hslGreen);
+  strip.SetPixelColor(2, hslBlue);
+  strip.SetPixelColor(3, hslWhite);
+  strip.Show();
 
-    
-    delay(3000);
+  
+  delay(3000);
 
-    // turn off the pixels
-    strip.SetPixelColor(0, hslBlack);
-    strip.SetPixelColor(1, hslBlack);
-    strip.SetPixelColor(2, hslBlack);
-    strip.SetPixelColor(3, hslBlack);
-    strip.Show();
+  // turn off the pixels
+  strip.SetPixelColor(0, hslBlack);
+  strip.SetPixelColor(1, hslBlack);
+  strip.SetPixelColor(2, hslBlack);
+  strip.SetPixelColor(3, hslBlack);
+  strip.Show();
 }
 
 
