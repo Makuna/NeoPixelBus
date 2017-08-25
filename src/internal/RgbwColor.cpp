ac3f9e7945a81aaa7a628690496859d4dfaa0d84
stbkde <bluxtend87@gmail.com>
2017-08-25 12:19:01 +0200

    Update RgbwColor.cpp

--- src/internal/RgbwColor.cpp
+++ src/internal/RgbwColor.cpp
@@ -189,3 +189,8 @@ RgbwColor RgbwColor::BilinearBlend(const RgbwColor& c00,
         c00.B * v00 + c10.B * v10 + c01.B * v01 + c11.B * v11,
         c00.W * v00 + c10.W * v10 + c01.W * v01 + c11.W * v11 );
 }
+
+String RgbwColor::toString(const char colorDivider) const
+{
+	return String(R) + colorDivider + String(G) + colorDivider + String(B) + colorDivider + String(W);
+}
