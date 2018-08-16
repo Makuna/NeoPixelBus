d87455f0c88b99a11b05d15a89be80e28b62e457
Michael Miller <Makuna@hotmail.com>
2018-08-16 12:49:07 -0700

    SwapPixelColor

--- src/NeoPixelBus.h
+++ src/NeoPixelBus.h
@@ -322,7 +322,14 @@ public:
         }
     }
     
+    void SwapPixelColor(uint16_t indexPixelOne, uint16_t indexPixelTwo)
+    {
+        auto colorOne = GetPixelColor(indexPixelOne);
+        auto colorTwo = GetPixelColor(indexPixelTwo);
 
+        SetPixelColor(indexPixelOne, colorTwo);
+        SetPixelColor(indexPixelTwo, colorOne);
+    };
  
 protected:
     const uint16_t _countPixels; // Number of RGB LEDs in strip
