e689afd131fd4ef2dd1a438b2ecfbfa488565ed1
Michael Miller <Makuna@hotmail.com>
2023-01-14 08:50:01 -0800

    correct timimg

--- src/internal/NeoPixelAvr.c
+++ src/internal/NeoPixelAvr.c
@@ -654,7 +654,7 @@ void send_data_32mhz_800(uint8_t* data, size_t sizeData, volatile uint8_t* port,
     // similar but NOT an exact copy of the prior 400-on-8 code.
 
     // 40 inst. clocks per bit: HHHHHxxxxxxxxLLLLLLL
-    // ST instructions:         ^   ^        ^       (T=0,5,13)
+    // ST instructions:         ^   ^        ^       (T=0,10,26)
 
     volatile uint8_t next;
     volatile uint8_t bit;
@@ -670,33 +670,39 @@ void send_data_32mhz_800(uint8_t* data, size_t sizeData, volatile uint8_t* port,
         "sbrc %[byte],  7"         "\n\t" // 1-2  if(b & 128)
         "mov  %[next], %[hi]"     "\n\t" // 0-1   next = hi    (T =  4)
         "dec  %[bit]"              "\n\t" // 1    bit--         (T =  5)
-        "st   %a[port],  %[next]"  "\n\t" // 2    PORT = next   (T =  7)
-        "mov  %[next] ,  %[lo]"    "\n\t" // 1    next = lo     (T =  8)
+
+        "nop"                      "\n\t" // 1    nop           (T = 6)
+        "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 8)
+        "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 10)
+
+        "st   %a[port],  %[next]"  "\n\t" // 2    PORT = next   (T =  12)
+
+        "mov  %[next] ,  %[lo]"    "\n\t" // 1    next = lo     (T =  13)
         "breq nextbyte20"          "\n\t" // 1-2  if(bit == 0) (from dec above)
-        "rol  %[byte]"             "\n\t" // 1    b <<= 1       (T = 10)
-        "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 12)
-        "nop"                      "\n\t" // 1    nop           (T = 13)
-        "st   %a[port],  %[lo]"    "\n\t" // 2    PORT = lo     (T = 15)
+        "rol  %[byte]"             "\n\t" // 1    b <<= 1       (T = 15)
+
         "nop"                      "\n\t" // 1    nop           (T = 16)
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 18)
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 20)
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 22)
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 24)
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 26)
-        "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 28)
+
+        "st   %a[port],  %[lo]"    "\n\t" // 2    PORT = lo     (T = 28)
+
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 30)
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 32)
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 34)
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 36)
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 38)
+
         "rjmp head20"              "\n\t" // 2    -> head20 (next bit out)
-        "nextbyte20:"               "\n\t" //                    (T = 10)
-        "ldi  %[bit]  ,  8"        "\n\t" // 1    bit = 8       (T = 11)
-        "ld   %[byte] ,  %a[ptr]+" "\n\t" // 2    b = *ptr++    (T = 13)
-        "st   %a[port], %[lo]"     "\n\t" // 2    PORT = lo     (T = 15)
-        "nop"                      "\n\t" // 1    nop           (T = 16)
-        "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 18)
-        "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 20)
+
+        "nextbyte20:"               "\n\t" //                   (T = 15)
+        "ldi  %[bit]  ,  8"        "\n\t" // 1    bit = 8       (T = 16)
+        "ld   %[byte] ,  %a[ptr]+" "\n\t" // 2    b = *ptr++    (T = 18)
+        "st   %a[port], %[lo]"     "\n\t" // 2    PORT = lo     (T = 20)
+
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 22)
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 24)
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 26)
@@ -705,7 +711,9 @@ void send_data_32mhz_800(uint8_t* data, size_t sizeData, volatile uint8_t* port,
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 32)
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 34)
         "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 36)
+
         "sbiw %[count], 1"         "\n\t" // 2    i--           (T = 38)
+
         "brne head20"             "\n"   // 2    if(i != 0) -> (next byte)
         : [port] "+e" (port),
         [byte]  "+r" (b),
