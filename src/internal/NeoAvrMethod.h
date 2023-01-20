ec720c46e56646a548474eb39eff86c69b9c670f
Michael Miller <Makuna@hotmail.com>
2023-01-19 16:10:15 -0800

    32Mhz 400Kbps

--- src/internal/NeoAvrMethod.h
+++ src/internal/NeoAvrMethod.h
@@ -40,6 +40,7 @@ extern "C"
     void send_data_16mhz_800(uint8_t* data, size_t sizeData, volatile uint8_t* port, uint8_t pinMask);
     void send_data_16mhz_400(uint8_t* data, size_t sizeData, volatile uint8_t* port, uint8_t pinMask);
     void send_data_32mhz_800(uint8_t* data, size_t sizeData, volatile uint8_t* port, uint8_t pinMask);
+    void send_data_32mhz_400(uint8_t* data, size_t sizeData, volatile uint8_t* port, uint8_t pinMask);
 }
 
 class NeoAvrSpeed800KbpsBase
@@ -63,9 +64,9 @@ public:
 #endif // PORTD
             send_data_12mhz_800_PortB(data, sizeData, pinMask);
 
-#elif (F_CPU >= 15400000UL) && (F_CPU <= 19000000L)  // 16Mhz CPU
+#elif (F_CPU >= 15400000UL) && (F_CPU <= 19000000UL)  // 16Mhz CPU
         send_data_16mhz_800(data, sizeData, port, pinMask);
-#elif (F_CPU >= 31000000UL) && (F_CPU <= 35000000L)  // 32Mhz CPU
+#elif (F_CPU >= 31000000UL) && (F_CPU <= 35000000UL)  // 32Mhz CPU
         send_data_32mhz_800(data, sizeData, port, pinMask);
 #else
 #error "CPU SPEED NOT SUPPORTED"
@@ -115,8 +116,10 @@ public:
 #elif (F_CPU >= 11100000UL) && (F_CPU <= 14300000UL)  // 12Mhz CPU
         send_data_12mhz_400(data, sizeData, port, pinMask);
 
-#elif (F_CPU >= 15400000UL) && (F_CPU <= 19000000L)  // 16Mhz CPU
+#elif (F_CPU >= 15400000UL) && (F_CPU <= 19000000UL)  // 16Mhz CPU
         send_data_16mhz_400(data, sizeData, port, pinMask);
+#elif (F_CPU >= 31000000UL) && (F_CPU <= 35000000UL)  // 32Mhz CPU
+        send_data_32mhz_400(data, sizeData, port, pinMask);
 #else
 #error "CPU SPEED NOT SUPPORTED"
 #endif
--- src/internal/NeoPixelAvr.c
+++ src/internal/NeoPixelAvr.c
@@ -520,7 +520,7 @@ void send_data_12mhz_400(uint8_t* data, size_t sizeData, volatile uint8_t* port,
         [ptr]    "e" (ptr));
 }
 
-#elif (F_CPU >= 15400000UL) && (F_CPU <= 19000000L)  // 16Mhz CPU
+#elif (F_CPU >= 15400000UL) && (F_CPU <= 19000000UL)  // 16Mhz CPU
 
 void send_data_16mhz_800(uint8_t* data, size_t sizeData, volatile uint8_t* port, uint8_t pinMask)
 {
@@ -641,7 +641,8 @@ void send_data_16mhz_400(uint8_t* data, size_t sizeData, volatile uint8_t* port,
         [lo]     "r" (lo));
 }
 
-#elif (F_CPU >= 31000000UL) && (F_CPU <= 35000000L)  // 32Mhz CPU
+#elif (F_CPU >= 31000000UL) && (F_CPU <= 35000000UL)  // 32Mhz CPU
+
 void send_data_32mhz_800(uint8_t* data, size_t sizeData, volatile uint8_t* port, uint8_t pinMask)
 {
     volatile uint16_t i = (uint16_t)sizeData; // Loop counter
@@ -724,6 +725,63 @@ void send_data_32mhz_800(uint8_t* data, size_t sizeData, volatile uint8_t* port,
         [hi]     "r" (hi),
         [lo]     "r" (lo));
 }
+
+// same as send_data_16mhz_800
+void send_data_32mhz_400(uint8_t* data, size_t sizeData, volatile uint8_t* port, uint8_t pinMask)
+{
+    volatile uint16_t i = (uint16_t)sizeData; // Loop counter
+    volatile uint8_t* ptr = data; // Pointer to next byte
+    volatile uint8_t b = *ptr++;    // Current byte value
+    volatile uint8_t hi;            // PORT w/output bit set high
+    volatile uint8_t lo;            // PORT w/output bit set low
+
+    // WS2811 and WS2812 have different hi/lo duty cycles; this is
+    // similar but NOT an exact copy of the prior 400-on-8 code.
+
+    // 20 inst. clocks per bit: HHHHHxxxxxxxxLLLLLLL
+    // ST instructions:         ^   ^        ^       (T=0,5,13)
+
+    volatile uint8_t next;
+    volatile uint8_t bit;
+
+    hi = *port | pinMask;
+    lo = *port & ~pinMask;
+    next = lo;
+    bit = 8;
+
+    asm volatile(
+        "head20:"                   "\n\t" // Clk  Pseudocode    (T =  0)
+        "st   %a[port],  %[hi]"    "\n\t" // 2    PORT = hi     (T =  2)
+        "sbrc %[byte],  7"         "\n\t" // 1-2  if(b & 128)
+        "mov  %[next], %[hi]"     "\n\t" // 0-1   next = hi    (T =  4)
+        "dec  %[bit]"              "\n\t" // 1    bit--         (T =  5)
+        "st   %a[port],  %[next]"  "\n\t" // 2    PORT = next   (T =  7)
+        "mov  %[next] ,  %[lo]"    "\n\t" // 1    next = lo     (T =  8)
+        "breq nextbyte20"          "\n\t" // 1-2  if(bit == 0) (from dec above)
+        "rol  %[byte]"             "\n\t" // 1    b <<= 1       (T = 10)
+        "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 12)
+        "nop"                      "\n\t" // 1    nop           (T = 13)
+        "st   %a[port],  %[lo]"    "\n\t" // 2    PORT = lo     (T = 15)
+        "nop"                      "\n\t" // 1    nop           (T = 16)
+        "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 18)
+        "rjmp head20"              "\n\t" // 2    -> head20 (next bit out)
+        "nextbyte20:"               "\n\t" //                    (T = 10)
+        "ldi  %[bit]  ,  8"        "\n\t" // 1    bit = 8       (T = 11)
+        "ld   %[byte] ,  %a[ptr]+" "\n\t" // 2    b = *ptr++    (T = 13)
+        "st   %a[port], %[lo]"     "\n\t" // 2    PORT = lo     (T = 15)
+        "nop"                      "\n\t" // 1    nop           (T = 16)
+        "sbiw %[count], 1"         "\n\t" // 2    i--           (T = 18)
+        "brne head20"             "\n"   // 2    if(i != 0) -> (next byte)
+        : [port] "+e" (port),
+        [byte]  "+r" (b),
+        [bit]   "+r" (bit),
+        [next]  "+r" (next),
+        [count] "+w" (i)
+        : [ptr] "e" (ptr),
+        [hi]     "r" (hi),
+        [lo]     "r" (lo));
+}
+
 #else
 #error "CPU SPEED NOT SUPPORTED"
 #endif
